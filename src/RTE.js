e4327bbb24c7568d76eadb095df9943eef15a22e
DavidHotaran <hotaran.d98@gmail.com>
2023-01-19 12:33:02 -0500

    complete component

--- src/RTE.js
+++ src/RTE.js
@@ -1,5 +1,44 @@
 import { useEffect, useRef, useState } from "react";
-import OPTIONS from "./options";
+import { TypeBold, TypeItalic, TypeUnderline, ListOl, ListUl, TypeH1, TypeH2, TypeH3, Fonts } from "react-bootstrap-icons";
+
+const OPTIONS = [
+    {
+        "command": "bold",
+        "icon": TypeBold
+    },
+    {
+        "command": "italic",
+        "icon": TypeItalic
+    },
+    {
+        "command": "underline",
+        "icon": TypeUnderline
+    },
+    {
+        "command": "insertOrderedList",
+        "icon": ListOl
+    },
+    {
+        "command": "insertUnorderedList",
+        "icon": ListUl
+    },
+    {
+        "command": "H1",
+        "icon": TypeH1
+    },
+    {
+        "command": "H2",
+        "icon": TypeH2
+    },
+    {
+        "command": "H3",
+        "icon": TypeH3
+    },
+    {
+        "command": "T",
+        "icon": Fonts
+    },
+];
 
 const RTE = () => {
     const inputFocus = useRef(null);
@@ -53,8 +92,6 @@ const RTE = () => {
                 <div contentEditable spellCheck={false} className="h-[338px] border-none outline-none p-1" ref={inputFocus}>
                 </div>
             </div>
-            <button className="bg-green-500 rounded-lg p-2" onClick={() => setOutput(inputFocus.current.innerHTML)}>submit</button>
-            <div dangerouslySetInnerHTML={{__html:output}}></div>
         </>
     );
 };
